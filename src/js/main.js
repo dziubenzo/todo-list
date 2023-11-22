fe698b52202dc0706cf7d8581547c9caaf50bef9
dziubenzo <dziubenzo@gmail.com>
2023-11-22 10:59:07 +0100

    Load GitHub logo from HTML template instead of JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,4 @@
 import '../css/main.scss';
-import Task from './tasks';
-import logo from '../assets/github-mark-white.svg';
+import { tasks } from './tasks';
 
-// Get images working
-const logoImg = document.querySelector('footer img');
-logoImg.src = logo;
-logoImg.alt = 'GitHub Logo - White';
-
-const tasks = [];
-
-const item1 = new Task(
-  'Learn JS',
-  'Learn some JS, dumbass!!!',
-  'default',
-  'high'
-);
-const item2 = new Task(
-  'Learn React',
-  'Learn some React, dumbass',
-  'default',
-  'medium'
-);
-const item3 = new Task(
-  'Learn algorithms',
-  'Learn some algorithms, dumbass',
-  'default',
-  'low'
-);
-
-tasks.push(item1, item2, item3);
-
-console.table(tasks);
+console.log(tasks);
--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,8 +1,8 @@
-export default class Task {
+class Task {
   creationDate;
-  dueDate
+  dueDate;
   completed;
-  completionDate
+  completionDate;
 
   constructor(title, description, list, priority) {
     this.title = title;
@@ -14,3 +14,28 @@ export default class Task {
     this.creationDate = this.creationDate.toLocaleString('en-GB');
   }
 }
+
+const tasks = [];
+
+const item1 = new Task(
+  'Learn JS',
+  'Learn some JS, dumbass!!!',
+  'default',
+  'high'
+);
+const item2 = new Task(
+  'Learn React',
+  'Learn some React, dumbass',
+  'default',
+  'medium'
+);
+const item3 = new Task(
+  'Learn algorithms',
+  'Learn some algorithms, dumbass',
+  'default',
+  'low'
+);
+
+tasks.push(item1, item2, item3);
+
+export { Task, tasks };
