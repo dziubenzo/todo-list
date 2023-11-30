0f94c3d91a09457bfddd89da74e95d47f5ba1fac
dziubenzo <dziubenzo@gmail.com>
2023-11-30 15:34:11 +0100

    Add a demo task
    It should only show once per device.

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,19 @@ import {
 import { handleTabs, generateListTabs } from './sidebar';
 import { createAddListButton } from './DOM';
 
+// Create a demonstration task once per device
+if (!localStorage.getItem('tasks')) {
+  Task.tasks.push(
+    new Task(
+      "I'm a demo task",
+      "Yes. I am indeed a demo task. You will probably delete me, right? Sad, but that's just the way life goes...",
+      'Personal',
+      'low',
+      new Date(2025, 11, 24)
+    )
+  );
+}
+
 getColourThemeFromLocalStorage();
 getTasksFromLocalStorage();
 getListsFromLocalStorage();
